24c89d73e889fac94b875e5d0711225d8e144c2e
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-06-28 12:09:36 +0530

    Refactor import statements in App component for consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Index from "./pages/Index";
-<<<<<<< HEAD
-import About from "./pages/about.jsx";
-import Login from "./pages/Login";
-=======
 import About from "./pages/About";
 import Articles from  "./pages/Articles";
 import Article  from  "./pages/Article";
->>>>>>> 623472d (Enhance website SEO and content structure; add articles feature with dynamic routing)
 
 function App() {
   return (
